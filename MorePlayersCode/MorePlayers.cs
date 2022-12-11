b35604a17f2164b213af0f2e4841de5a033fbe1a
Matthew Aydlett <aydlettm@yahoo.com>
2022-12-10 20:21:26 -0500

    Add files via upload

---
+++ MorePlayersCode/MorePlayers.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+
+
+namespace MorePlayers
+{
+    public class Main: MelonMod
+    {
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (sceneName == "MainMenu")
+            {
+                GameNetworkManager.MaxPlayersPerRoom = 15;
+                LoggerInstance.Msg("WARNING: No testing has been done past 7 players so crashes or lag may occur past 7 players!");
+            }
+        }
+    }
+}
